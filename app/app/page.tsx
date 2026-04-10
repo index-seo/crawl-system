32d48560ebb0610ae2c4a162f62cd564684b74c7
index-seo <kmrazib8.5@gmail.com>
2026-04-10 14:21:03 +0600

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: `
+      <h1 style="text-align:center; margin-top:50px;">Authority Crawl Injection System</h1>
+      <p style="text-align:center;">আপনার ইনডেক্সিং সিস্টেমটি এখানে কাজ করবে।</p>
+    ` }} />
+  );
+}
